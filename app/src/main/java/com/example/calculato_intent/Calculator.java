df9137773a2908603a6af495bed242a130976c9a
RelinDmitrii <relin-dmitrii@yandex.ru>
2021-02-11 20:38:11 +0300

    Initial commit

---
+++ app/src/main/java/com/example/calculato_intent/Calculator.java
@@ -0,0 +1,26 @@
+package com.example.calculato_intent;
+
+public class Calculator {
+
+    public long Calculate(String first, String second, String operator) {
+
+        long firstNumber = Long.parseLong(first);
+        long secondNumber = Long.parseLong(second);
+
+        switch (operator) {
+            case "+":
+                return firstNumber + secondNumber;
+            case "-":
+                return firstNumber - secondNumber;
+            case "*":
+                return firstNumber * secondNumber;
+            case "/":
+                if (secondNumber == 0) {
+                    return 0;
+                }
+                return firstNumber / secondNumber;
+        }
+        return 0;
+    }
+
+}
